d95fc7202bdfb8e259834347aaa24fd57cb4c9d3
Tom Saeger <tlsaeger@icloud.com>
2021-08-17 12:45:30 +0200

    added shuffle to elemnts

--- components/TheTools.vue
+++ components/TheTools.vue
@@ -73,6 +73,17 @@ export default {
       noContent: false,
     };
   },
+  beforeMount(){
+  let array = this.filteredArray;
+   for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+  return array;
+  },
   methods: {
     filtering(e, group) {
       let activeValues = [];
