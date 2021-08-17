9af68809e1da32c78adfcaad6890d41a4fb02b9c
Tom Saeger <tlsaeger@icloud.com>
2021-08-17 10:52:29 +0200

    fixed content issue by disablign babel

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,19 +58,19 @@ export default {
   
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    babel: {
-      presets({isServer}) {
-        const targets = isServer ? { node: 'current' } : { ie: 11 }
-        return [
-          [ require.resolve("@babel/preset-env"), { targets }  ]
-        ]
-      },
-      plugins: [
-        "@babel/syntax-dynamic-import",
-        "@babel/transform-runtime",
-        "@babel/transform-async-to-generator"
-      ]
-    },
+    // babel: {
+    //   presets({isServer}) {
+    //     const targets = isServer ? { node: 'current' } : { ie: 11 }
+    //     return [
+    //       [ require.resolve("@babel/preset-env"), { targets }  ]
+    //     ]
+    //   },
+    //   plugins: [
+    //     "@babel/syntax-dynamic-import",
+    //     "@babel/transform-runtime",
+    //     "@babel/transform-async-to-generator"
+    //   ]
+    // },
   }
 
 };
