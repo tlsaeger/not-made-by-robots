6930ee653ff746c4e9b16a2d9791606a1abe1441
Tom Saeger <tlsaeger@icloud.com>
2022-02-05 18:18:08 +0100

    added imprint

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,6 +1,43 @@
 <template>
-    <div class="footer-wrapper">
-        <div>About</div>
-        <div>Impressum</div>
-    </div>
+  <div class="footer-wrapper">
+    <div>About</div>
+    <div @click="showImprint = true">Impressum</div>
+    <transition name="from-corner">
+      <TheImprint
+        v-if="showImprint"
+        v-on:closeTheImprint="closeSomething('Imprint')"
+      />
+    </transition>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showImprint: false
+    };
+  },
+  methods: {
+    closeSomething(name) {
+      if (name === "Imprint") {
+        this.showImprint = false;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* TODO Good animation */
+.from-corner-enter-active,
+.from-corner-leave-active {
+  transition: all 0.5s;
+  transform: scale(0.5);
+}
+.from-corner-enter,
+.from-corner-leave-to {
+  transform: scale(0.5);
+  opacity: 0;
+}
+</style>
