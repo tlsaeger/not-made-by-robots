4a623f74d9cff3118d6c97a601735552bac91187
Tom Saeger <tlsaeger@icloud.com>
2022-02-06 14:37:55 +0100

    Added About

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="footer-wrapper">
-    <div>About</div>
+    <div @click="showAbout = true">About</div>
     <div @click="showImprint = true">Impressum</div>
     <transition name="from-corner">
       <TheImprint
         v-if="showImprint"
         v-on:closeTheImprint="closeSomething('Imprint')"
       />
+      <TheAbout v-if="showAbout" v-on:closeTheAbout="closeSomething('About')" />
     </transition>
   </div>
 </template>
@@ -15,13 +16,16 @@
 export default {
   data() {
     return {
-      showImprint: false
+      showImprint: false,
+      showAbout: false
     };
   },
   methods: {
     closeSomething(name) {
       if (name === "Imprint") {
         this.showImprint = false;
+      } else if (name === "About") {
+        this.showAbout = false;
       }
     }
   }
@@ -29,15 +33,20 @@ export default {
 </script>
 
 <style scoped>
+.f .footer-wrapper > div {
+  cursor: pointer;
+}
 /* TODO Good animation */
 .from-corner-enter-active,
 .from-corner-leave-active {
   transition: all 0.5s;
-  transform: scale(0.5);
+  transform: scale(1) translateY(0) translateX(0);
 }
 .from-corner-enter,
 .from-corner-leave-to {
-  transform: scale(0.5);
+  transform: scale(0) translateX(-200%) translateY(200%);
   opacity: 0;
 }
+/* @keyframes corner {
+} */
 </style>
