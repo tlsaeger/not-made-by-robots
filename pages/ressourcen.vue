b9dfda67bac45a23cb6678f5e5c234db8a3f78fb
Tom Saeger <tlsaeger@icloud.com>
2022-03-07 11:13:49 +0100

    preparing multi lang

--- pages/ressourcen.vue
+++ pages/ressourcen.vue
@@ -8,7 +8,7 @@ export default {
   async asyncData({ $content, params, error }) {
     let ressourcen, wissen, tutorials;
     try {
-      ressourcen = await $content("ressourcen", params.slug).fetch();
+      ressourcen = await $content("ressourcen", "de", params.slug).fetch();
       wissen = await $content("wissen", params.slug).fetch();
       tutorials = await $content("tutorial", params.slug).fetch();
       // OR const article = await $content(`articles/${params.slug}`).fetch()
